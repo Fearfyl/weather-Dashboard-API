c5ec368630637741cc9979bfa7236302ed353d3a
Fearfyl <wadebuchanan18@gmail.com>
2025-02-27 18:35:24 -0500

    Fixed file path to client dist folder

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,13 +4,17 @@ dotenv.config();
 
 // Import the routes
 import routes from './routes/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-app.use(express.static('client/dist'));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
